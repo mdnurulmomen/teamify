de672255b36fae9dac6ae1996c68464615a4012c
Scopic Interviewing <noreply@codeaid.io>
2023-01-08 05:07:58 +0000

    Initial commit

---
+++ app/Http/Controllers/PlayerController.php
@@ -0,0 +1,36 @@
+<?php
+
+// /////////////////////////////////////////////////////////////////////////////
+// PLEASE DO NOT RENAME OR REMOVE ANY OF THE CODE BELOW. 
+// YOU CAN ADD YOUR CODE TO THIS FILE TO EXTEND THE FEATURES TO USE THEM IN YOUR WORK.
+// /////////////////////////////////////////////////////////////////////////////
+
+namespace App\Http\Controllers;
+
+class PlayerController extends Controller
+{
+    public function index()
+    {
+        return response("Failed", 500);
+    }
+
+    public function show()
+    {
+        return response("Failed", 500);
+    }
+
+    public function store()
+    {
+        return response("Failed", 500);
+    }
+
+    public function update()
+    {
+        return response("Failed", 500);
+    }
+
+    public function destroy()
+    {
+        return response("Failed", 500);
+    }
+}
