feaba4d98e1fbc3b2b4986e3c262f19fc8a7319b
Momen <mdnurulmomen.bd@gmail.com>
2023-01-11 13:06:33 +0600

    Adding validations at formrequest

--- app/Http/Requests/PlayerRequest.php
+++ app/Http/Requests/PlayerRequest.php
@@ -8,6 +8,13 @@ use Illuminate\Foundation\Http\FormRequest;
 class PlayerRequest extends FormRequest
 {
     /**
+     * Indicates if the validator should stop on the first rule failure.
+     *
+     * @var bool
+     */
+    protected $stopOnFirstFailure = true;
+
+    /**
      * Determine if the user is authorized to make this request.
      *
      * @return bool
@@ -32,7 +39,7 @@ class PlayerRequest extends FormRequest
             ],
             'playerSkills' => 'required|array|min:1',
             'playerSkills.*.skill' => [
-                'required', 'string',
+                'required', 'string', 'distinct',
                 Rule::in(['defense', 'attack', 'speed', 'strength', 'stamina']),
             ],
             'playerSkills.*.value' => [
@@ -49,33 +56,27 @@ class PlayerRequest extends FormRequest
     public function messages()
     {
         return [
-            'name' => [
-                'required' => 'Player name is required',
-                '*' => 'Invalid string for player name'
-            ],
+            'name.required' => 'The :attribute field is missing',
+            'name.*' => 'Invalid value for :attribute: :input',   // string
 
-            'position' => [
-                'required' => 'Player position field is required',
-                'in' => 'Player positon is invalid (allowed:defender, midfielder, forward)',
-                '*' => 'Invalid value for position'
-            ],
+            'position.required' => 'The :attribute field is missing',
+            // 'in' => 'The :attribute must be one of the following types: :values',
+            'position.*' => 'Invalid value for :attribute: :input',     // string / in
 
-            'playerSkills' => [
-                'required' => 'The :attribute field is required',
-                'array' => 'Player skill should be an array',
-                'min' => 'The :attribute should have at least one element',
-            ],
+            'playerSkills.required' => 'The :attribute field is missing',
+            'playerSkills.array' => 'The :attribute should be an array',
+            'playerSkills.min' => 'The :attribute should have at least one skill',
+            'playerSkills.*' => 'Invalid value for :attribute: :input',
 
-            'playerSkills.*.skill' => [
-                'required' => 'The :attribute field is required',
-                'string' => 'The :attribute should be a string',
-                'in' => 'The :attribute is invalid (allowed:defense, attack, speed, strength, stamina)',
-            ],
+            'playerSkills.*.skill.required' => 'The :attribute field is missing',
+            'playerSkills.*.skill.distinct' => 'The :attribute field is duplicate',
+            // 'playerSkills.*.skill.string' => 'The :attribute should be a string',
+            // 'playerSkills.*.skill.in' => 'The :attribute is invalid (allowed:defense, attack, speed, strength, stamina)',
+            'playerSkills.*.skill.*' => 'Invalid value for :attribute: :input',     // string / in
 
-            'playerSkills.*.value' => [
-                'integer' => 'The :attribute should be an integer',
-                'min' => 'The minimum value of :attribute is 0',
-            ],
+            // 'playerSkills.*.value.integer' => 'The :attribute should be an integer',
+            // 'playerSkills.*.value.min' => 'The minimum value of :attribute is 0',
+            'playerSkills.*.value.*' => 'Invalid value for :attribute: :input',
         ];
     }
 }
--- app/Http/Requests/TeamRequest.php
+++ app/Http/Requests/TeamRequest.php
@@ -8,6 +8,13 @@ use Illuminate\Foundation\Http\FormRequest;
 class TeamRequest extends FormRequest
 {
     /**
+     * Indicates if the validator should stop on the first rule failure.
+     *
+     * @var bool
+     */
+    protected $stopOnFirstFailure = true;
+
+    /**
      * Determine if the user is authorized to make this request.
      *
      * @return bool
@@ -40,6 +47,33 @@ class TeamRequest extends FormRequest
     }
 
     /**
+    * Configure the validator instance.
+    *
+    * @param  \Illuminate\Validation\Validator  $validator
+    * @return void
+    */
+    public function withValidator($validator)
+    {
+        $validator->after(function ($validator) {
+
+            foreach ($this->all() as $requirementKey => $requirement) {
+
+                foreach ($this->all() as $requirementNewKey => $requirementNew) {
+
+                    if ($requirementKey != $requirementNewKey && $requirement['position'] == $requirementNew['position'] && $requirement['mainSkill'] == $requirementNew['mainSkill']) {
+
+                        $validator->errors()->add("$requirementNewKey.position", "Duplicate value for position: ".$requirementNew['position']." and mainSkill: ".$requirementNew['mainSkill']);
+
+                    }
+
+                }
+
+            }
+
+        });
+    }
+
+    /**
      * Get the error messages for the defined validation rules.
      *
      * @return array
@@ -47,23 +81,20 @@ class TeamRequest extends FormRequest
     public function messages()
     {
         return [
-            '*.position' => [
-                'required' => 'Player position field is required',
-                'string' => 'The :attribute should be a string',
-                'in' => 'Player positon is invalid (allowed:defender, midfielder, forward)'
-            ],
+            '*.position.required' => 'The :attribute field is missing',
+            // 'string' => 'The :attribute should be a string',
+            // 'in' => 'The :attribute must be one of the following types: :values',
+            '*.position.*' => 'Invalid value for :attribute: :input',      // string / in
 
-            '*.mainSkill' => [
-                'required' => 'The :attribute field is required',
-                'string' => 'The :attribute should be a string',
-                'in' => 'The :attribute is invalid (allowed:defense, attack, speed, strength, stamina)'
-            ],
+            '*.mainSkill.required' => 'The :attribute field is missing',
+            // 'string' => 'The :attribute should be a string',
+            // 'in' => 'The :attribute must be one of the following types: :values',
+            '*.mainSkill.*' => 'Invalid value for :attribute: :input',      // string / in
 
-            '*.numberOfPlayers' => [
-                'required' => 'The :attribute field is required',
-                'integer' => 'The :attribute should be an integer',
-                'min' => 'The minimum value of :attribute is 0',
-            ]
+            '*.numberOfPlayers.required' => 'The :attribute field is missing',
+            // 'integer' => 'The :attribute should be an integer',
+            // 'min' => 'The minimum value of :attribute is 0',
+            '*.numberOfPlayers.*' => 'Invalid value for :attribute: :input'     // integer / min
         ];
     }
 }
